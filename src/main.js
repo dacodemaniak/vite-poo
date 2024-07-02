9ea3937aae14c298ac822162a40e493e36b603ba
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2024-07-02 07:58:24 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+var Main = /** @class */ (function () {
+    function Main() {
+        console.log("Hello TS");
+    }
+    return Main;
+}());
+(function () {
+    new Main();
+})();
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+class Main {
+  constructor() {
+    console.log(`Hello TS`)
+  }
+}
+
+(() => {
+  new Main()
+})()
