9c6b6404bf22a1c23c06c531c8493025d7821246
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2024-07-04 13:17:58 +0200

    Shape specifications

---
+++ tests/abstract-shape.test.ts
@@ -0,0 +1,13 @@
+describe(`Abstract Shape class`, () => {
+    it(`Should be an instance of Shape`, () => {
+
+    })
+
+    it(`Should be an instance of Rectangle`, () => {
+
+    })
+
+    it(`Should have a perimeter of 40`, () => {
+        
+    })
+})
